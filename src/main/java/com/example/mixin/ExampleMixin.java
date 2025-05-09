062a3f90eb669d436c4ca0c23a052eaab285dfe4
dannytaylor <daniel@mclegoman.com>
2025-05-10 04:50:17 +1200

    Modified for use with Quilt and Minecraft in-20100223 (OrnitheMC)

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,16 @@
 package com.example.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.client.C_5664496;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(MinecraftServer.class)
+@Mixin(C_5664496.class) // C_5664496 = MinecraftClient
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+	@Inject(method = "run", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GameGui;<init>(Lnet/minecraft/client/C_5664496;)V"))
+	private void init(CallbackInfo ci) {
+		// This code is injected after GameGui has been initialized in MinecraftClient.run()V
+		System.out.println("BAZINGA");
 	}
 }
